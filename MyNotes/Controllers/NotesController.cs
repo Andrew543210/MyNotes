69ba45f70495214de031e542bbd5a7b32e5d50b3
ANDRII KHRUSHCH <maybug.jr@gmail.com>
2026-03-24 22:50:52 +0200

    NotesCintroller fixed

--- MyNotes/Controllers/NotesController.cs
+++ MyNotes/Controllers/NotesController.cs
@@ -31,7 +31,7 @@ public class NotesController : ControllerBase
     public async Task<IActionResult> Get([FromQuery]GetNotesRequest request, CancellationToken ct)
     {
         var notesQuery =  _dbContext.Notes
-            .Where(n => !string.IsNullOrWhiteSpace(request.Search) &&
+            .Where(n => string.IsNullOrWhiteSpace(request.Search) ||
                         n.Title.ToLower().Contains(request.Search.ToLower()));
 
         Expression<Func<Note, object>> selectorKey = request.SortItem?.ToLower() switch
